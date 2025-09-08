f58970ed2a49a299c18898f345af714fd86d9dfb
BrendanGoddard <bjgoddard21@gmail.com>
2025-09-08 17:27:06 -0400

    Added secure computing lab #1

---
+++ 3068-SecureComputing/Labs/lab02/helloworld.php
@@ -0,0 +1,11 @@
+<html>
+    <head><title>Welcome to b_goddard198580's page</title></head>
+    <body>
+        <h1>"Hello World! from b_goddard198580</h1>
+        <pre>
+        <?php 
+            print_r($_SERVER);
+        ?>
+        </pre>
+    </body>
+    </html>
