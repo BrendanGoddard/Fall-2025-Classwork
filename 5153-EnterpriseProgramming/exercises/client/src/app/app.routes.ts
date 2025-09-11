593130fdb717ccb00446c9b706260d0bb72055cf
BrendanGoddard <bjgoddard21@gmail.com>
2025-09-11 17:04:05 -0400

    added lab 3 part A work completed

--- 5153-EnterpriseProgramming/exercises/client/src/app/app.routes.ts
+++ 5153-EnterpriseProgramming/exercises/client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from '../app/home/home';
+import { EmployeeHome } from './employee/employee-home/employee-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, },
+
+  // TODO: Replace Home for EmployeeHome component once its created
+  { path: 'employees', component: EmployeeHome, },
+];
