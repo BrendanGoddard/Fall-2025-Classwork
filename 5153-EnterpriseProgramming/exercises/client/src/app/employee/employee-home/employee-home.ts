a2ddd99695077a177a525e0201943f2818abc1f4
BrendanGoddard <bjgoddard21@gmail.com>
2025-09-17 17:14:18 -0400

    added starting for 5103

--- 5153-EnterpriseProgramming/exercises/client/src/app/employee/employee-home/employee-home.ts
+++ 5153-EnterpriseProgramming/exercises/client/src/app/employee/employee-home/employee-home.ts
@@ -1,32 +1,58 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
+import { EmployeeDetails } from '../employee-details/employee-details';
+import { EMPLOYEE_DEFAULT } from '../../constants'
 
 @Component({
   selector: 'app-employee-home',
-  imports: [MatCardModule, MatListModule],
+  imports: [MatCardModule, MatListModule, EmployeeDetails],
   templateUrl: './employee-home.html',
   styleUrl: './employee-home.scss'
 })
 export class EmployeeHome implements OnInit {
 
-  employees = signal<Employee[]>([]);
+  employees: WritableSignal<Employee[]> = signal<Employee[]>([]);
+  employeeInDetail: WritableSignal<Employee> = signal<Employee>(EMPLOYEE_DEFAULT)
 
   constructor(public employeeService: EmployeeService) {
   }
 
   ngOnInit(): void {
-    this.employeeService.get().subscribe({
-      next: (payload: any) => {
+    this.refresh();
+  }
+
+  refresh(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (payload: Employee[]) => {
         console.log(payload);
-        this.employees.set(payload._embedded.employees)
+        this.employees.set(payload);
       },
       error: (e: Error) => console.error(e),
-      complete: () => console.log('Complete!')
+      complete: () => this.selectEmployee(EMPLOYEE_DEFAULT)
+    });
+  }
+  selectEmployee(employee: Employee) {
+    this.employeeInDetail.set(employee);
+  }
+
+  
+  hasEmployeeSelected() : boolean {
+    return this.employeeInDetail().id > 0;
+  }
+
+    updateEmployee(employee: Employee) {
+    this.employeeService.updateEmployee(employee).subscribe({
+      next: (payload: Employee) => {
+        console.log(payload);
+        this.employeeInDetail.set(payload);
+      },
+      error: (e: Error) => console.error(e),
+      complete: () => this.refresh()
     });
   }
 }
--- 5153-EnterpriseProgramming/exercises/client/src/app/employee/employee.service.ts
+++ 5153-EnterpriseProgramming/exercises/client/src/app/employee/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,13 @@ export class EmployeeService {
 
   get(): Observable<any> {
     return this.http.get(`http://localhost:8080/employees`);
+  }
+  
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`http://localhost:8080/api/employees`);
+  }
+
+   updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`http://localhost:8080/api/employees`, employee);
   }
 }
