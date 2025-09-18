54861cc1363559b63353e1444e9648aa4798d489
BrendanGoddard <bjgoddard21@gmail.com>
2025-09-18 18:14:12 -0400

    added secure computing week 3 files

--- 3068-SecureComputing/Labs/lab02/helloworld.php
+++ 3068-SecureComputing/Labs/lab02/helloworld.php
@@ -1,7 +1,7 @@
 <html>
     <head><title>Welcome to b_goddard198580's page</title></head>
     <body>
-        <h1>"Hello World! from b_goddard198580</h1>
+        <h1>"Hello World! from b_goddard198580 </h1>
         <pre>
         <?php 
             print_r($_SERVER);
