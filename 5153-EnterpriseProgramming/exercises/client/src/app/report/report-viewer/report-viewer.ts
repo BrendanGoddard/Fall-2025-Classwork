f8fc5dbe181a615865b899a7e78049314e5372a2
BrendanGoddard <bjgoddard21@gmail.com>
2025-11-24 20:23:17 -0500

    added in dropdown for reports

--- 5153-EnterpriseProgramming/exercises/client/src/app/report/report-viewer/report-viewer.ts
+++ 5153-EnterpriseProgramming/exercises/client/src/app/report/report-viewer/report-viewer.ts
@@ -1,36 +1,37 @@
 import { Component, signal } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 import { Employee } from '@app/employee/employee';
 import { EmployeeDropdown } from '@app/employee/employee-dropdown/employee-dropdown';
 
-import { Report } from '@app/report/report';
-import { ReportService } from '@app/report/report-service';
+import { Report } from '../report';
+import { ReportItem } from '../report-item';
+import { ReportDropdown } from '../report-dropdown/report-dropdown';
+
+import { REPORT_DEFAULT } from '@app/constants';
 
 @Component({
   selector: 'app-report-viewer',
-  imports: [ReactiveFormsModule, MatCardModule, MatSelectModule, MatOptionModule, EmployeeDropdown],
+  imports: [MatCardModule, EmployeeDropdown, ReportDropdown],
   templateUrl: './report-viewer.html',
   styleUrl: './report-viewer.scss'
 })
 export class ReportViewer {
 
-  constructor(protected reportService: ReportService) {
+  constructor() {
   }
 
-  viewerForm: FormGroup = new FormGroup({
-    employeeId: new FormControl(),
-  });
+  employeeId = signal<number>(0);
+  report = signal<Report>(REPORT_DEFAULT);
 
   employeeSelected(employee: Employee) {
-      this.reportService.getAllById(employee.id).subscribe({
-      next: (payload: Report[]) => console.log(payload),
-      error: e => console.log(e),
-    });
+    this.employeeId.set(employee.id);
+    this.report.set(REPORT_DEFAULT);
+  }
+
+  reportSelected(report: Report) {
+    console.log(report);
+    this.report.set(report);
   }
 }
